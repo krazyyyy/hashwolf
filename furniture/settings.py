06d43edaa0825f3cc9b68bbfe67cc46354deed2a
Krazy0 <57904740+krazyyyy@users.noreply.github.com>
2020-11-23 10:51:17 +0500

    Update settings.py

--- furniture/settings.py
+++ furniture/settings.py
@@ -83,7 +83,7 @@ AUTH_USER_MODEL = 'frontapp.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"),
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
